4c8056b38c6138865d96b9f897d072761ac7b094
Rebecca Lind <32701610+rebeccalind@users.noreply.github.com>
2017-11-14 17:10:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var url = 'http://api.open-notify.org/iss-now.json';
-// API from http://open-notify.org/Open-Notify-API/ISS-Location-Now/
+console.log("API from http://open-notify.org/Open-Notify-API/ISS-Location-Now/");
 
 fetch(url)
 
